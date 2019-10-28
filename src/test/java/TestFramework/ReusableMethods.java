d0e21b8083f1c13830525fceafb461a4699d27e1
sdey0104 <sourav-dey@gmx.com>
2019-10-28 22:51:08 +0100

    Rest Test Moneyyou

---
+++ src/test/java/TestFramework/ReusableMethods.java
@@ -0,0 +1,23 @@
+package TestFramework;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+
+public class ReusableMethods {
+	
+
+	public static JsonPath rawToJson(Response r)
+	{ 
+		String respon=r.asString();
+		JsonPath x=new JsonPath(respon);
+		return x;
+	}
+
+	
+	
+
+}
